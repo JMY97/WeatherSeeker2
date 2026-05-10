83f11708a8eb1dda16696cab4e6e508cedfc4555
JMY <jymontana97@gmail.com>
2026-05-10 15:48:21 -0500

    Views/Repo Clean up

--- WebApp1/Program.cs
+++ WebApp1/Program.cs
@@ -59,6 +59,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=About}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
